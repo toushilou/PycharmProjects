9a10ba9d0cc42b5d5e6e7b62314870e3c418e3d2
toushilou <seekyq@gmail.com>
2015-04-15 17:39:34 +0800

    modify some files

--- MySpider/spider_1/spider_1.py
+++ MySpider/spider_1/spider_1.py
@@ -1,11 +1,11 @@
 __author__ = 'qyuan'
-
+# coding=utf-8
 import requests
 import csv
 
 r = requests.get('http://ichart.yahoo.com/table.csv?s=600000.SS&a=08&b=25&c=2010&d=09&e=8&f=2010&g=d')
-reader = csv.reader(file('106.csv', 'rb'))
+reader = csv.reader(file('106.csv', 'r+'))
 for line in reader:
-    print line[0]
+    print line
 
 print r._content
