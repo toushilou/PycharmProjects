fb07e735b4a45fbf692995ec120bb2e8fe25e9ac
toushilou <seekyq@gmail.com>
2015-04-15 13:44:55 +0800

    Initial commit

---
+++ MySpider/spider_1/spider_1.py
@@ -0,0 +1,11 @@
+__author__ = 'qyuan'
+
+import requests
+import csv
+
+r = requests.get('http://ichart.yahoo.com/table.csv?s=600000.SS&a=08&b=25&c=2010&d=09&e=8&f=2010&g=d')
+reader = csv.reader(file('106.csv', 'rb'))
+for line in reader:
+    print line[0]
+
+print r._content
