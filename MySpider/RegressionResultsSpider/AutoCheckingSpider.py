054c570fef2531a4ea4e8c8b3e71a89d82d89200
toushilou <seekyq@gmail.com>
2015-06-01 15:11:46 +0800

    regression checker

--- MySpider/RegressionResultsSpider/AutoCheckingSpider.py
+++ MySpider/RegressionResultsSpider/AutoCheckingSpider.py
@@ -1,6 +1,14 @@
 __author__ = 'qyuan'
 
 import lxml.etree  as etree
+import PyQt4.QtGui as QtGui
+
+import sys
+app = QtGui.QApplication(sys.argv)
+widget = QtGui.QWidget()
+widget.resize(250, 150)
+widget.setWindowTitle('simple')
+widget.show()
 
 xmlFile = open('1_CN_BJ_Case_002.ref', 'r')
 root = etree.fromstring(xmlFile.read())
@@ -11,6 +19,7 @@ for child in children:
     for node in childNode:
         attributes = node.attrib
         for attrib in attributes:
-            print attributes[attrib]
+            print attrib, '==', attributes[attrib]
         print '---------------'
     print '**************'
+sys.exit(app.exec_())
