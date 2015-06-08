70dbff1f06b0ccb5d4639e0ab256a443d356a608
toushilou <seekyq@gmail.com>
2015-06-08 17:15:01 +0800

    regression checker

--- MySpider/RegressionResultsSpider/AutoCheckingSpider.py
+++ MySpider/RegressionResultsSpider/AutoCheckingSpider.py
@@ -2,6 +2,7 @@ __author__ = 'qyuan'
 
 import lxml.etree  as etree
 import PyQt4.QtGui as QtGui
+from Datatypes import Content
 
 import sys
 app = QtGui.QApplication(sys.argv)
@@ -13,22 +14,25 @@ widget.show()
 ignoreList = ['SID', 'SpeedLimit', 'RoadType', 'RoadSubType', 'RoadPriority', 'Tunnel', 'LaneNumber', 'TrafficDirection', 'Gradient', 'TimeZoneIndex', 'IsBoundingEdge']
 rgcCondition = []
 contentCondition = []
+contentDict = {}
 #guidanceCondition = []
-
+forwardConnectEdge, backwardConnectEdge, edgePoint = '', '', ''
 xmlFile = open('1_CN_BJ_Case_002.ref', 'r')
 root = etree.fromstring(xmlFile.read())
 children = list(root)
 for child in children:
-    print child.attrib['rule']
     childNode = list(child)
     for node in childNode:
         attributes = node.attrib
-        for attrib in attributes:
-            if attributes[attrib] not in ignoreList:
-                print attrib, '==', attributes[attrib]
+        nodeName = attributes['name']
+        if nodeName == 'ForwardConnectEdge':
+            forwardConnectEdge = attributes['value']
+        elif nodeName == 'BackwardConnectEdge':
+            backwardConnectEdge = attributes['value']
+        elif nodeName == 'EdgePoint':
+            edgePoint = attributes['value']
+    contentDict[edgePoint] = Content.Content(forwardConnectEdge, backwardConnectEdge, edgePoint)
 
-            else:
-                break
-        print '---------------'
-    print '**************'
+for k, v in contentDict.items():
+    print k, ' == ', v.toString()
 sys.exit(app.exec_())
--- MySpider/RegressionResultsSpider/Datatypes/Content.py
+++ MySpider/RegressionResultsSpider/Datatypes/Content.py
@@ -5,3 +5,11 @@ class Content:
         self.forwardConnectEdge = forwardConnectEdge
         self.backwardConnectEdge = backwardConnectEdge
         self.edgePoint = edgePoint
+
+    def toString(self):
+        return 'forwardConnectEdge == ' \
+               + self.forwardConnectEdge \
+               + 'backwardConnectEdge == ' \
+               + self.backwardConnectEdge \
+               + 'edgePoint == ' \
+               + self.edgePoint
