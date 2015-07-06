6276edf720a5601df6ef543ea211e4b99c1875d1
toushilou <seekyq@gmail.com>
2015-07-06 23:59:41 +0800

    MP spider

--- MySpider/WeChatHack/SogouSpider.py
+++ MySpider/WeChatHack/SogouSpider.py
@@ -11,9 +11,29 @@ queryStr = {}
 queryStr['query'] = u'新闻'
 r = requests.get(url, params = queryStr)
 soup = BeautifulSoup(r._content)
-for item in soup.find_all(attrs = 'txt-box'):
-    print item.encode('gbk')
-    name =  item.find(name='h3').encode('gbk')
-    number = item.find(attrs='span')
-    print number
 
+class MPObject:
+    def __init__(self, mp_name, mp_number, mp_description, mp_biz):
+        self.mp_name = mp_name
+        self.mp_number = mp_number
+        self.mp_description = mp_description
+        self.mp_biz = mp_biz
+
+
+mp_dic = {}
+for item in soup.find_all(attrs = 'txt-box'):
+    print item
+    mp_name =  item.find(name = 'h3').text
+    mp_number = item.find(name = 'span').text[4:]
+    mp_description = item.find(name = 'span', attrs = {'class': 'sp-txt'}).text
+    try:
+        latest_article = item.find(name = 'a', attrs = {'class': 'blue'})['href']
+        mp_biz = latest_article[len('http://mp.weixin.qq.com/s?__biz='):]
+        index = mp_biz.find('&')
+        mp_biz = mp_biz[:index]
+        print mp_biz
+    except Exception, e:
+        print e.message
+        continue
+    mp_object = MPObject(mp_name, mp_number, mp_description, mp_biz)
+    mp_dic[mp_name] = mp_object
