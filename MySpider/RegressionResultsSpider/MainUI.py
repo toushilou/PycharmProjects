60fe17c52f828c119d8cbd83f3a6b4b0b823b8f7
toushilou <seekyq@gmail.com>
2015-08-21 17:34:11 +0800

    scrapy

--- MySpider/RegressionResultsSpider/MainUI.py
+++ MySpider/RegressionResultsSpider/MainUI.py
@@ -2,16 +2,32 @@ __author__ = 'qyuan'
 import sys
 from PyQt4 import QtGui
 from PyQt4 import QtCore
-class MainWindow(QtGui.QWidget):
+
+class contentWindow(QtGui.QWidget):
+    def __init__(self):
+        QtGui.QWidget.__init__(self, parent = None)
+        self.resize(400, 300)
+        self.setWindowTitle('Content')
+        self.setWindowIcon(QtGui.QIcon('./icons/telenav.png'))
+
+class MainWindow(QtGui.QMainWindow):
     def __init__(self, parent = None):
         QtGui.QWidget.__init__(self, parent)
         self.resize(400, 300)
         self.setWindowTitle('Regression Analyst')
         self.setWindowIcon(QtGui.QIcon('./icons/telenav.png'))
-        quit = QtGui.QPushButton('Close', self)
-        quit.setGeometry(10, 10, 60, 35)
-        self.connect(quit, QtCore.SIGNAL('clicked()'), QtGui.qApp, QtCore.SLOT('quit()'))
+        check_content = QtGui.QPushButton('Content', self)
+        check_content.setGeometry(10, 30, 60, 35)
+        # exitAction = QtGui.QAction(QtGui.QIcon('exit.png'), '&Exit', self)
+        # exitAction.setShortcut('Ctrl+Q')
+        # exitAction.setStatusTip('Exit application')
+        # exitAction.triggered.connect(QtGui.qApp.quit)
+        self.statusBar().showMessage('Ready')
+        self.connect(check_content, QtCore.SIGNAL('clicked()'), self, QtCore.SLOT("newWindow()"))
         self.center()
+        # menubar = self.menuBar()
+        # file = menubar.addMenu('&File')
+        # file.addAction(exitAction)
     def closeEvent(self, event):
         reply = QtGui.QMessageBox.question(self, 'Message',
             "Are you sure to quit?", QtGui.QMessageBox.Yes,
@@ -26,3 +42,12 @@ class MainWindow(QtGui.QWidget):
         size = self.geometry()
         self.move((screen.width() - size.width()) / 2,
         (screen.height() - size.height()) / 2)
+    @QtCore.pyqtSlot()
+    def newWindow(self):
+        self.cw = contentWindow()
+        self.cw.show()
+
+app = QtGui.QApplication(sys.argv)
+qb = MainWindow()
+qb.show()
+sys.exit(app.exec_())
