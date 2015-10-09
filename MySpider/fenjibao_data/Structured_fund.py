1e61b2af7199bf1f61707e3d1ccc044361cf9d8c
toushilou <seekyq@gmail.com>
2015-10-09 17:23:16 +0800

    fenjibao

--- MySpider/fenjibao_data/Structured_fund.py
+++ MySpider/fenjibao_data/Structured_fund.py
@@ -7,12 +7,11 @@ __author__ = 'qyuan'
 # page, extra_resources = session.open('http://www.abcfund.cn/style/fundlist.php')
 # print session.content
 
-f = open('fund_data', 'r')
+f = open('fund_data', 'rU')
 w = open('clean_data', 'w')
 c = ''
 s = ''
 flag = False
-counter = 0
 while c != '|':
     if r'<td>' in s or r'</td>' in s:
         s = ''
@@ -20,15 +19,32 @@ while c != '|':
     c = f.read(1)
     if c == r'<' and flag == True:
         if s != '' and '%' not in s:
-            # w.write(s.strip())
-            # if counter < 6:
+            # # w.write(s.strip())
+            # if counter < 11:
+            #     if counter != 2 and counter != 3:
+            #         w.write(s.strip())
+            #         w.write(' ')
             #     counter += 1
-            #     w.write(s.strip())
-            #     w.write(' ')
             # else:
             #     w.write('\r\n')
             #     counter = 0
-            print s
+            w.write(s.strip())
+            w.write('\n')
         s = ''
         flag = False
     s += c
+
+f = open('clean_data', 'rU')
+w = open('info', 'w')
+counter = 0
+
+for str in f:
+    if counter <= 10:
+        if counter != 2 and counter != 3:
+            w.write(str.strip())
+            w.write('|')
+        counter += 1
+    else:
+        counter = 1
+        w.write('\n')
+        w.write(str.strip())
